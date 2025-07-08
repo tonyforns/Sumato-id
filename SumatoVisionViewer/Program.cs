2360cc06e8ba8a7cce9d6bcc36b9e896ab543a1e
Tony Forns <iaforns@gmail.com>
2025-07-08 14:43:38 -0300

    SetUp SumatoVisionViwer project

---
+++ SumatoVisionViewer/Program.cs
@@ -0,0 +1,13 @@
+namespace SumatoVisionViewer
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
