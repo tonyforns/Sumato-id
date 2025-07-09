809d6a9314125939bcbe9077a7ba1defb3348946
Tony Forns <iaforns@gmail.com>
2025-07-09 12:36:34 -0300

    FrameQueue implement IDispose

--- SumatoVisionCore/FrameQueue.cs
+++ SumatoVisionCore/FrameQueue.cs
@@ -2,10 +2,29 @@ using System.Collections.Concurrent;
 
 namespace SumatoVisionCore;
 
-public class FrameQueue
+public class FrameQueue : IDisposable
 {
     private BlockingCollection<IFrame> _queue = new();
     public int Count => _queue.Count;
     public void PushQueue(IFrame frame) => _queue.Add(frame);
     public IFrame PullQueue() => _queue.Take();
+
+    public void Dispose()
+    {
+        if (_queue != null)
+        {
+            _queue.CompleteAdding();
+
+            foreach (IFrame frame in _queue)
+            {
+                if(frame.Frame is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+            }
+
+            _queue.Dispose();
+            _queue = null;
+        }
+    }
 }
