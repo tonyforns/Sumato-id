b40a3eb977cee46e0e17e5e7d548974416c32025
Tony Forns <iaforns@gmail.com>
2025-07-09 20:10:26 -0300

    FrameProcessor implement IThreadTask

--- SumatoVisionCore/FrameProcessor.cs
+++ SumatoVisionCore/FrameProcessor.cs
@@ -1,28 +1,8 @@
 ﻿namespace SumatoVisionCore;
 public class FrameProcessor : ThreadHandler
 {
-    private readonly FrameQueue _queue;
-    private Action<IFrame> _processFrameAction;
-
     public FrameProcessor(FrameQueue queue, Action<IFrame> processFrameAction)
     {
-        _queue = queue;
-        _processFrameAction = processFrameAction;
-    }
-
-    internal override void ThreadFunction()
-    {
-        if (_queue.Count == 0)
-        {
-            return;
-        }
-        IFrame frame = _queue.PullQueue();
-        _processFrameAction?.Invoke(frame);
-    }
-
-    internal override void OnStopped()
-    {
-        _processFrameAction = null;
-        base.OnStopped();
+        _threadTask = new ProcessFrameTask(queue, processFrameAction);
     }
 }
---
+++ SumatoVisionCore/ThreadTasks/ProcessFrameTask.cs
@@ -0,0 +1,26 @@
+﻿namespace SumatoVisionCore;
+
+public class ProcessFrameTask : IThreadTask
+{
+    private readonly FrameQueue _queue;
+    private Action<IFrame> _processFrameAction;
+
+    public ProcessFrameTask(FrameQueue queue, Action<IFrame> processFrameAction)
+    {
+        _queue = queue;
+        _processFrameAction = processFrameAction;
+    }
+    public void Task()
+    {
+        if (_queue.Count == 0)
+        {
+            return;
+        }
+        IFrame frame = _queue.PullQueue();
+        _processFrameAction?.Invoke(frame);
+    }
+    public void OnStopped()
+    {
+        _processFrameAction = null;
+    }
+}
