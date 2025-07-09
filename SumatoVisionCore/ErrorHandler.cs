88d5632988879abc5b9ee638d0e0f07bc75398d0
Tony Forns <iaforns@gmail.com>
2025-07-08 21:46:16 -0300

    Add base ErrrorHandler

---
+++ SumatoVisionCore/ErrorHandler.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SumatoVisionCore;
+
+public static class ErrorHandler
+{
+    public static Action<string>? OnError;
+       public static void LogError(string message)
+       {
+           OnError?.Invoke(message);
+    }
+}
