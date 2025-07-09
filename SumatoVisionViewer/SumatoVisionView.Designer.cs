90dbe99d855399962117a581f51ed3b2b07195bc
Tony Forns <iaforns@gmail.com>
2025-07-09 12:44:33 -0300

    Improve SumatoVisionView buttons positions and height with cons

--- SumatoVisionViewer/SumatoVisionView.Designer.cs
+++ SumatoVisionViewer/SumatoVisionView.Designer.cs
@@ -4,6 +4,8 @@ using SumatoVisionViewer.Properties;
 namespace SumatoVisionViewer;
 partial class SumatoVisionView
 {
+    const int BTN_HEIGHT = 25;
+
     private System.ComponentModel.IContainer components = null;
     private PictureBox pictureBox;
     private Button startBtn;
@@ -27,67 +29,55 @@ partial class SumatoVisionView
         stopBtn = new Button();
         ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
         SuspendLayout();
-        // 
-        // pictureBox
-        // 
-        pictureBox.Dock = DockStyle.Fill;
+
+        pictureBox.Dock = DockStyle.Top; 
         pictureBox.Location = new Point(0, 0);
         pictureBox.Name = "pictureBox";
-        pictureBox.Size = new Size(640, 455);
+        pictureBox.Size = new Size(SetUpConfig.ResizeWidth, SetUpConfig.ResizeHeight);
         pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
         pictureBox.TabIndex = 0;
         pictureBox.TabStop = false;
-        // 
-        // startBtn
-        // 
         pictureBox.Image = Resources.logo;
+
         startBtn.Dock = DockStyle.Bottom;
-        startBtn.Location = new Point(0, 455);
+        startBtn.Location = new Point(0, SetUpConfig.ResizeHeight);
         startBtn.Name = "startBtn";
-        startBtn.Size = new Size(640, 25);
+        startBtn.Size = new Size(SetUpConfig.ResizeWidth, BTN_HEIGHT);
         startBtn.TabIndex = 1;
         startBtn.Text = "Start";
         startBtn.UseVisualStyleBackColor = true;
         startBtn.Click += startBtn_Click;
         startBtn.Visible = false;
-        // 
-        // fileVideoBtn
-        // 
+  
         fileVideoBtn.Dock = DockStyle.Bottom;
-        fileVideoBtn.Location = new Point(0, 430);
+        fileVideoBtn.Location = new Point(0, SetUpConfig.ResizeHeight);
         fileVideoBtn.Name = "fileVideoBtn";
-        fileVideoBtn.Size = new Size(640, 25);
+        fileVideoBtn.Size = new Size(SetUpConfig.ResizeWidth, BTN_HEIGHT);
         fileVideoBtn.TabIndex = 2;
         fileVideoBtn.Text = "Video File";
         fileVideoBtn.UseVisualStyleBackColor = true;
         fileVideoBtn.Click += fileVideoBtn_Click;
-        // 
-        // cameraBtn
-        // 
+     
         cameraBtn.Dock = DockStyle.Bottom;
-        cameraBtn.Location = new Point(0, 405);
+        cameraBtn.Location = new Point(0, SetUpConfig.ResizeHeight + BTN_HEIGHT);
         cameraBtn.Name = "cameraBtn";
-        cameraBtn.Size = new Size(640, 25);
+        cameraBtn.Size = new Size(SetUpConfig.ResizeWidth, BTN_HEIGHT);
         cameraBtn.TabIndex = 3;
         cameraBtn.Text = "Camera";
         cameraBtn.UseVisualStyleBackColor = true;
         cameraBtn.Click += cameraBtn_Click;
-        // 
-        // stopBtn
-        // 
+   
         stopBtn.Dock = DockStyle.Bottom;
-        stopBtn.Location = new Point(0, 380);
+        stopBtn.Location = new Point(0, SetUpConfig.ResizeHeight);
         stopBtn.Name = "button1";
-        stopBtn.Size = new Size(640, 25);
+        stopBtn.Size = new Size(SetUpConfig.ResizeWidth, BTN_HEIGHT);
         stopBtn.TabIndex = 4;
         stopBtn.Text = "Stop";
         stopBtn.UseVisualStyleBackColor = true;
         stopBtn.Click += stopBtn_Click;
         stopBtn.Visible = false;
-        // 
-        // Form1
-        // 
-        ClientSize = new Size(640, 480);
+ 
+        ClientSize = new Size(SetUpConfig.ResizeWidth, SetUpConfig.ResizeHeight + + (BTN_HEIGHT * 2));
         Controls.Add(stopBtn);
         Controls.Add(cameraBtn);
         Controls.Add(fileVideoBtn);
@@ -98,4 +88,5 @@ partial class SumatoVisionView
         ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
         ResumeLayout(false);
     }
+
 }
