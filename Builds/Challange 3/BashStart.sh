926377cac2710d06b2edeada4bdb74f7b63a8314
Tony Forns <iaforns@gmail.com>
2025-07-10 15:44:19 -0300

    Remove builds

--- Builds/Challange 3/BashStart.sh
+++
@@ -1,7 +0,0 @@
-BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-"$BASE_DIR/ServiceQueue/ServiceQueue.exe" &
-"$BASE_DIR/ServiceCapture/ServiceCapture.exe" &
-"$BASE_DIR/ServiceProcessing/ServiceProcessing.exe" &
-
-wait
