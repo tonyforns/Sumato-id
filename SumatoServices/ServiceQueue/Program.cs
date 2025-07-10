e23c3f5e7eb681784beadec1985a6d4f59ee3ab7
Tony Forns <iaforns@gmail.com>
2025-07-10 13:01:20 -0300

    Create Base SericeQueue

---
+++ SumatoServices/ServiceQueue/Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using System.Net.WebSockets;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using System.Linq;
+using System.Threading.Tasks;
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+ConcurrentQueue<byte[]> frameQueue = new();
+List<WebSocket> processingClients = new();
+
+app.UseWebSockets();
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/ws")
+    {
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            Console.WriteLine("WebSocket connected.");
+
+            if (context.Request.Query["role"] == "capture")
+            {
+                var buffer = new byte[1024 * 1024];
+                while (true)
+                {
+                    var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Close)
+                        break;
+
+                    var frameData = buffer[..result.Count];
+                    frameQueue.Enqueue(frameData);
+
+                    foreach (var client in processingClients.ToList())
+                    {
+                        if (client.State == WebSocketState.Open)
+                        {
+                            await client.SendAsync(new ArraySegment<byte>(frameData), WebSocketMessageType.Binary, true, CancellationToken.None);
+                        }
+                    }
+                }
+            }
+            else if (context.Request.Query["role"] == "processing")
+            {
+                processingClients.Add(webSocket);
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    await Task.Delay(100); // mantener conexión abierta
+                }
+                processingClients.Remove(webSocket);
+            }
+        }
+        else
+        {
+            context.Response.StatusCode = 400;
+        }
+    }
+    else
+    {
+        await next();
+    }
+});
+
+app.Run("http://localhost:5001");
