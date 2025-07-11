8c183e070c0dddb30dd78d7a724f9bcf30049bce
Tony Forns <iaforns@gmail.com>
2025-07-11 11:06:31 -0300

    Create IFrameSource from webSocket server

---
+++ SumatoVisionCore/FrameSources/ServerFrameSource.cs
@@ -0,0 +1,41 @@
+﻿using OpenCvSharp;
+
+namespace SumatoVisionCore;
+public class ServerFrameSource : IFrameSource
+{
+    private readonly FrameQueue queue;
+
+    public ServerFrameSource(string url)
+    {
+        queue = new FrameQueue();
+        WebSocketClient webSocketClient = new WebSocketClient(url);
+
+        webSocketClient.ConnectAsync().Wait();
+        webSocketClient.ReceiveMessagesAsync(OnMessageReceived);
+    }
+
+    private void OnMessageReceived(MessageModel model)
+    {
+        queue.PushQueue(ReconstructFrame(model));
+    }
+
+    public static IFrame ReconstructFrame(MessageModel message)
+    {
+        return message.DataType switch
+        {
+            MessageModel.FrameType.Mat => new MatFrame(Cv2.ImDecode(message.Data, ImreadModes.Color)),
+            _ => throw new NotSupportedException()
+        };
+    }
+
+    public bool Read(out IFrame frame)
+    {
+        frame = null;   
+        if (queue.Count > 0)
+        {
+            frame = queue.PullQueue();
+            return true;
+        }
+        return false;
+    }
+}
