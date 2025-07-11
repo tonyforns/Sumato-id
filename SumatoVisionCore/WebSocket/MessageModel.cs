e4236797c52d7b5ecdde79b479a5005716996af7
Tony Forns <iaforns@gmail.com>
2025-07-11 11:06:05 -0300

    Add webscoket client

---
+++ SumatoVisionCore/WebSocket/MessageModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SumatoVisionCore;
+public class MessageModel
+{
+    public byte[] Data { get; set; }
+    public FrameType DataType { get; set; }
+
+    public MessageModel(byte[] data, FrameType frameType)
+    {
+        Data = data;
+        DataType = frameType;
+    }
+
+    public enum FrameType
+    {
+        Mat,
+        BitMap
+    }
+
+}
---
+++ SumatoVisionCore/WebSocket/WebSocketClient.cs
@@ -0,0 +1,89 @@
+﻿using System.Net.WebSockets;
+
+namespace SumatoVisionCore;
+public class WebSocketClient
+{
+    private ClientWebSocket _webSocket;
+    private readonly Uri _serverUri;
+
+    public WebSocketClient(string uri)
+    {
+        _webSocket = new ClientWebSocket();
+        _serverUri = new Uri(uri);
+    }
+
+    public async Task ConnectAsync()
+    {
+        if (_webSocket.State != WebSocketState.Open)
+        {
+            await _webSocket.ConnectAsync(_serverUri, CancellationToken.None);
+            Console.WriteLine($"Connected to {_serverUri}");
+        }
+    }
+
+    public async Task SendMessageAsync(MessageModel message)
+    {
+        if (_webSocket.State == WebSocketState.Open)
+        {
+            var buffer = SerializeMessage(message);
+            await _webSocket.SendAsync(
+                new ArraySegment<byte>(buffer),
+                WebSocketMessageType.Binary,
+                endOfMessage: true,
+                cancellationToken: CancellationToken.None
+            );
+        }
+    }
+
+    public async Task ReceiveMessagesAsync(Action<MessageModel> onMessageReceived)
+    {
+        var buffer = new byte[1024 * 64];
+        while (_webSocket.State == WebSocketState.Open)
+        {
+            var result = await _webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer),
+                CancellationToken.None
+            );
+
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                Console.WriteLine("Service closed");
+                await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+            }
+            else if (result.MessageType == WebSocketMessageType.Binary)
+            {
+                var messageBytes = buffer.Take(result.Count).ToArray();
+                MessageModel message = DeserializeMessage(messageBytes);
+                onMessageReceived?.Invoke(message);
+            }
+        }
+    }
+
+    public async Task CloseAsync()
+    {
+        if (_webSocket.State == WebSocketState.Open || _webSocket.State == WebSocketState.CloseReceived)
+        {
+            await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client closed conextion", CancellationToken.None);
+            Console.WriteLine("WebSocket conection closed");
+        }
+    }
+    public byte[] SerializeMessage(MessageModel message)
+    {
+        using var ms = new MemoryStream();
+        using var bw = new BinaryWriter(ms);
+        bw.Write((int)message.DataType);
+        bw.Write(message.Data.Length);
+        bw.Write(message.Data);
+        return ms.ToArray();
+    }
+
+    public MessageModel DeserializeMessage(byte[] data)
+    {
+        using var ms = new MemoryStream(data);
+        using var br = new BinaryReader(ms);
+        var type = (MessageModel.FrameType)br.ReadInt32();
+        int length = br.ReadInt32();
+        var buffer = br.ReadBytes(length);
+        return new MessageModel(buffer, type);
+    }
+}
