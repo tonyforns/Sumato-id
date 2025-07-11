fe6a708d2144dd3bd38c1a8d5cd85c3bb091cd55
Tony Forns <iaforns@gmail.com>
2025-07-11 11:07:43 -0300

    Update Capture services to use webscoket client

---
+++ SumatoServices/ServiceCapture/CaptureService.cs
@@ -0,0 +1,35 @@
+﻿
+using OpenCvSharp;
+
+namespace SumatoVisionCore;
+internal class CaptureService
+{
+    private readonly WebSocketClient _websocketClient;
+    private FrameReader _frameReader;
+    private FrameProcessor _frameProcessor;
+
+    public CaptureService(string serverUri, IFrameSource frameSource)
+    {
+        _websocketClient = new WebSocketClient(serverUri);
+        var queue = new FrameQueue();
+        _frameReader = new FrameReader(queue, frameSource);
+
+
+        _frameProcessor = new FrameProcessor(queue, async (frame) =>
+        {
+            using Mat mat = (Mat)frame.Frame;
+            byte[] data = ((Mat)frame.Frame).ImEncode(".jpg");
+            var message = new MessageModel(data, MessageModel.FrameType.Mat);
+            Console.WriteLine($"Sending frame of size: {data.Length} bytes");
+            await _websocketClient.SendMessageAsync(message);
+        });
+    }
+
+
+    public void Start()
+    {
+        _websocketClient.ConnectAsync().Wait();
+        _frameReader.Start();
+        _frameProcessor.Start();
+    }
+}
--- SumatoServices/ServiceCapture/Program.cs
+++ SumatoServices/ServiceCapture/Program.cs
@@ -1,36 +1,11 @@
-using OpenCvSharp;
-using OpenCvSharp.Extensions;
-using System.Net.WebSockets;
-using System.Drawing.Imaging;
 using SumatoVisionCore;
 
 class Program
 {
-    static async Task Main()
+    static void Main(string[] args)
     {
-        using var ws = new ClientWebSocket();
-        await ws.ConnectAsync(new Uri("ws://localhost:5001/ws?role=capture"), CancellationToken.None);
+        CaptureService captureService = new CaptureService(SetUpConfig.CaptureServiceUri, new CameraFrameSoruce(0));
 
-        var queue = new FrameQueue();
-        var reader = new FrameReader(queue, new CameraFrameSoruce(0));
-
-        var processor = new ProcessFrameTask(queue, async (frame) =>
-        {
-            using Mat mat = (Mat)frame.Frame;
-            byte[] data = mat.ImEncode(".jpg");
-
-            if (ws.State == WebSocketState.Open)
-            {
-                Console.WriteLine($"Sending frame {mat} of size {data.Length} bytes.");
-                await ws.SendAsync(new ArraySegment<byte>(data), WebSocketMessageType.Binary, true, CancellationToken.None);
-            }
-        });
-
-        reader.Start();
-
-        while(true)
-        {
-            processor.Task();
-        }
+        captureService.Start();
     }
 }
