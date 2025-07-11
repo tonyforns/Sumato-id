efb84713cbdb8c7f38e32aa7dec50871de5b310d
Tony Forns <iaforns@gmail.com>
2025-07-11 11:08:48 -0300

    Update SericeProcessing to use ServerFrameSource using websocket

---
+++ SumatoServices/ServiceProcessing/ProcessingService.cs
@@ -0,0 +1,35 @@
+﻿namespace SumatoVisionCore;
+internal class ProcessingService
+{
+    private FrameReader _frameReader;
+    private FrameProcessor _frameProcessor;
+
+    public ProcessingService(string serverUri)
+    {
+        var queue = new FrameQueue();
+
+        IFrameSource frameSource = new ServerFrameSource(serverUri);
+        _frameReader = new FrameReader(queue, frameSource);
+
+        _frameProcessor = new FrameProcessor(queue, (frame) =>
+        {
+            if(!frame.IsEmpty)
+            {
+                frame.Resize(SetUpConfig.ResizeWidth, SetUpConfig.ResizeHeight);
+                Console.WriteLine($"Processing frame: {frame.Frame} Size {frame.Size.X} x {frame.Size.Y}");
+            }
+
+            if(frame.Frame is IDisposable disposable)
+            {
+                disposable.Dispose();    
+            }
+        });
+    }
+
+    public void Start()
+    {
+        _frameProcessor.Start();
+
+        _frameReader.Start();
+    }
+}
--- SumatoServices/ServiceProcessing/Program.cs
+++ SumatoServices/ServiceProcessing/Program.cs
@@ -1,30 +1,11 @@
-using System.Net.WebSockets;
-using OpenCvSharp;
 using SumatoVisionCore;
+
 class Program
 {
-    static async Task Main()
+    static void Main(string[] args)
     {
-        using var ws = new ClientWebSocket();
-        await ws.ConnectAsync(new Uri("ws://localhost:5001/ws?role=processing"), CancellationToken.None);
+        ProcessingService processingService = new ProcessingService(SetUpConfig.ProcessingServiceUri);
 
-        var buffer = new byte[1024 * 1024];
-
-        while (true)
-        {
-            var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            var frameData = buffer[..result.Count];
-
-            using Mat mat = Cv2.ImDecode(frameData, ImreadModes.Color);
-            if (mat.Empty())
-            {
-                Console.WriteLine("Error: Empty or invalid Image.");
-                continue;
-            }
-            MatFrame matFrame = new MatFrame(mat);
-            matFrame.Resize(640, 480);
-
-            Console.WriteLine($"Received frame: {matFrame.Frame}  {matFrame.Size.X}x{matFrame.Size.Y}");
-        }
+        processingService.Start();
     }
 }
